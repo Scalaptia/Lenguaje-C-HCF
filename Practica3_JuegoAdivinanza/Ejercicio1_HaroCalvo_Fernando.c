45c82fd5c9de96e589b3daa660e8a13ac1a20c12
Scalaptia <fernando@haroware.com>
2023-09-12 20:13:40 -0700

    Practica 3

---
+++ Practica3_JuegoAdivinanza/Ejercicio1_HaroCalvo_Fernando.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(NULL));                 // Inicializa generador de números aleatorios
+    int numSecreto = rand() % 100 + 1; // Escoger número
+    int intento, numIntentos = 0;
+    int op;
+
+inicio:
+    printf("Bienvenido al juego de adivinar el numero!\n\n");
+    printf("- Para ganar, debes escribir un numero entre el 1 y el 100\n");
+    printf("- Si el numero esta por debajo o por encima del correcto, tendras otro intento\n");
+    printf("- Si el numero es correcto, ganas!\n\n");
+    system("PAUSE");
+    system("CLS");
+
+adivinanza:
+    printf("Ingrese un numero entre 1 y 100: ");
+    scanf("%d", &intento);
+    numIntentos++;
+
+    if (intento < 1 || intento > 100)
+    {
+        printf("El numero debe estar entre 1 y 100.\n");
+        goto adivinanza;
+    }
+
+    if (intento < numSecreto)
+    {
+        printf("Demasiado bajo\n");
+        goto adivinanza;
+    }
+    else if (intento > numSecreto)
+    {
+        printf("Demasiado alto\n");
+        goto adivinanza;
+    }
+    else
+    {
+        printf("\nAdivinaste el numero %d en %d intentos\n", numSecreto, numIntentos);
+        system("PAUSE");
+        system("CLS");
+        goto reinicio;
+    }
+
+reinicio:
+    printf("Desea jugar otra vez? (1: Si / 2: No): ");
+    scanf("%d", &op);
+    system("CLS");
+
+    switch (op)
+    {
+    case 1:
+        numIntentos = 0;
+        numSecreto = rand() % 100 + 1;
+        goto adivinanza;
+        break;
+
+    case 2:
+        printf("Gracias por jugar!\n");
+        printf("Finalizando programa...");
+        return 0;
+
+    default:
+        goto reinicio;
+        break;
+    }
+}
