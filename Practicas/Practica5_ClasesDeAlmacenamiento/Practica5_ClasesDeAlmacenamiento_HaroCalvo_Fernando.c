fb4fa746f774d488c4a93436ab042eee92d55063
Scalaptia <fernando@haroware.com>
2023-09-25 22:47:59 -0700

    Practica 5 Carrera de Coches

--- Practicas/Practica5_ClasesDeAlmacenamiento/Practica5_ClasesDeAlmacenamiento_HaroCalvo_Fernando.c
+++ Practicas/Practica5_ClasesDeAlmacenamiento/Practica5_ClasesDeAlmacenamiento_HaroCalvo_Fernando.c
@@ -9,10 +9,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 // Prototipos de Funciones
+int generarNumeroLoteria(void);
+int generarVelocidadAleatoria(void);
 void loteria(void);
-void carrera(void);
+void iniciarCarrera(void);
 
 int main()
 {
@@ -36,14 +39,16 @@ int main()
             printf("   JUEGO DE LA LOTERIA \n");
             loteria();
 
+            printf("\n");
             system("PAUSE");
             break;
 
         case 2:
             // TITULO
             printf("   LA CARRERA DE COCHES \n");
-            carrera();
+            iniciarCarrera();
 
+            printf("\n");
             system("PAUSE");
             break;
         }
@@ -54,7 +59,7 @@ int main()
 
 /*
     Función: generarNumeroLoteria
-    Descripción: Genera un número aleatorio entre 1 y 100
+    Descripción: Genera un número aleatorio entre 1 y 100.
 */
 int generarNumeroLoteria()
 {
@@ -89,9 +94,60 @@ void loteria()
 }
 
 /*
-    Función: carrera
+    Función: iniciarCarrera
+    Descripción: Genera una velocidad aleatoria entre 100 y 200 km/h.
+*/
+int generarVelocidadAleatoria()
+{
+    return rand() % 101 + 100;
+}
+
+/*
+    Función: iniciarCarrera
     Descripción: Simula una carrera de coches utilizando variables de registro (register).
 */
-void carrera()
+void iniciarCarrera()
 {
+    int distancia = 1000;
+
+    char n1[20] = "Rayo McQueen";
+    char n2[20] = "Francesco Bernoulli";
+    char n3[20] = "Mate";
+
+    register int v1 = generarVelocidadAleatoria();
+    register int v2 = generarVelocidadAleatoria();
+    register int v3 = generarVelocidadAleatoria();
+
+    float t1 = (float)distancia / v1;
+    float t2 = (float)distancia / v2;
+    float t3 = (float)distancia / v3;
+
+    // Mostrar los resultados de la carrera
+    printf("\n%s - Velocidad: %d km/h - Tiempo: %.2fs\n", n1, v1, t1);
+    printf("%s - Velocidad: %d km/h - Tiempo: %.2fs\n", n2, v2, t2);
+    printf("%s - Velocidad: %d km/h - Tiempo: %.2fs\n\n", n3, v3, t3);
+
+    // Identificar al coche ganador
+    if (t1 < t2)
+    {
+        if (t1 < t3)
+        {
+            printf("%s es el ganador de la carrera!\n", n1);
+        }
+        else
+        {
+            printf("%s es el ganador de la carrera!\n", n3);
+        }
+    }
+    else
+    {
+        if (t2 < t3)
+        {
+            printf("%s es el ganador de la carrera!\n", n2);
+        }
+        else
+        {
+            printf("%s es el ganador de la carrera!\n", n3);
+        }
+    }
 }
