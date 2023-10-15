c5600d70fec98ec06a4395ac421bd4321c8a33c2
Scalaptia <fernando@haroware.com>
2023-10-15 11:51:50 -0700

    Taller 6

---
+++ Talleres/Taller6_Apuntadores/README.md
@@ -0,0 +1,8 @@
+# Taller 6. Apuntadores y Argumentos de Funciones
+
+Clase: Lenguaje C
+
+1. **Apuntadores Básicos:** Dado un entero x, crea un apuntador ptr que apunte a x. Luego, imprime el valor de x y el valor al que apunta ptr.
+2. **Pasar Apuntadores a una Función:** Escribe una función llamada multiplicarPorDos que tome un apuntador a un entero como argumento. Dentro de la función, multiplica el valor al que apunta el apuntador por 2. Luego, llama a esta función desde main() y muestra el valor modificado.
+3. **Arreglos:** Crea un arreglo de enteros de tamaño 7 (Los valores que genere sean random entre 1-50, y que los valores no sean repetidos). Luego, crea un apuntador que apunte al primer elemento del arreglo. Utiliza un bucle para imprimir todos los elementos del arreglo utilizando el apuntador.
+4. **Modificar Elementos de un Arreglo:** Escribe una función llamada sumarAElementos que tome un apuntador a un arreglo de enteros y un valor entero como argumentos. Dentro de la función, suma el valor entero a cada elemento del arreglo utilizando aritmética de direcciones. Llama a esta función desde main() y muestra el arreglo modificado.
--- Talleres/Taller6_Apuntadores/Taller6_Apuntadores.c
+++ Talleres/Taller6_Apuntadores/Taller6_Apuntadores.c
@@ -2,7 +2,120 @@
 #include <stdlib.h>
 #include <time.h>
 
+int numAleatorio(int ri, int rf);
+int busqSeqVect(int vect[], int n, int num);
 void multiplicarPorDos(int *x);
+void sumarAElementos(int *arr, int n, int val);
+
+int main()
+{
+    srand(time(NULL));
+    int op, i;
+    int num;
+    int x, *ptrx;
+    int y, *ptry;
+    int arr1[7], arr2[10];
+    int *ptrArr1, *ptrArr2;
+
+    do
+    {
+        system("CLS");
+        printf("   M  E   N   U \n");
+        printf("1.- APUNTADORES BASICOS \n");
+        printf("2.- PASAR APUNTADORES A UNA FUNCION \n");
+        printf("3.- ARREGLOS \n");
+        printf("4.- MODIFICAR ELEMENTOS EN UN ARREGLO \n");
+        printf("0.- SALIR  \n");
+        printf("\nESCOGE UNA OPCION: ");
+        scanf("%d", &op);
+        system("CLS");
+
+        switch (op)
+        {
+        case 1:
+            ptrx = &x;
+
+            x = 10;
+
+            printf("X: %d, *ptr: %d", x, *ptrx);
+            break;
+
+        case 2:
+            ptry = &y;
+
+            y = 10;
+
+            printf("Y antes: %d\n", y);
+
+            multiplicarPorDos(ptry);
+
+            printf("Y despues: %d", y);
+            break;
+
+        case 3:
+            for (i = 0; i < 7; i++)
+            {
+                do
+                {
+                    num = numAleatorio(0, 50);
+                } while (busqSeqVect(arr1, 7, num) != -1);
+
+                arr1[i] = num;
+            }
+
+            ptrArr1 = arr1;
+
+            for (i = 0; i < 7; i++)
+            {
+                printf("%d, ", *ptrArr1);
+                ptrArr1++;
+            }
+
+            break;
+
+        case 4:
+            ptrArr2 = arr2;
+
+            for (i = 0; i < 10; i++)
+            {
+                do
+                {
+                    num = numAleatorio(0, 50);
+                } while (busqSeqVect(arr2, 10, num) != -1);
+
+                arr2[i] = num;
+            }
+
+            printf("Arreglo original:   ");
+            for (i = 0; i < 10; i++)
+            {
+                printf("%d, ", *ptrArr2);
+                ptrArr2++;
+            }
+            ptrArr2 = arr2;
+
+            sumarAElementos(ptrArr2, 10, 5); // Modificar arreglo
+            ptrArr2 = arr2;
+
+            printf("\nArreglo modificado: ");
+            for (i = 0; i < 10; i++)
+            {
+                printf("%d, ", *ptrArr2);
+                ptrArr2++;
+            }
+
+            break;
+
+        case 0:
+            return 0;
+        }
+
+        printf("\n\n");
+        system("PAUSE");
+    } while (op != 0);
+}
+
+/* Funciones Auxiliares */
 
 int numAleatorio(int ri, int rf)
 {
@@ -26,85 +139,18 @@ int busqSeqVect(int vect[], int n, int num)
     return -1; // No se encontró
 }
 
-int main()
-{
-    srand(time(NULL));
-    int op, i;
-    do
-    {
-        system("CLS");
-        printf("   M  E   N   U \n");
-        printf("1.- APUNTADORES BASICOS \n");
-        printf("2.- PASAR APUNTADORES A UNA FUNCION \n");
-        printf("3.- ARREGLOS \n");
-        printf("4.- MODIFICAR ELEMENTOS EN UN ARREGLO \n");
-        printf("0.- SALIR  \n");
-        printf("ESCOGE UNA OPCION: ");
-        scanf("%d", &op);
-        system("CLS");
-
-        switch (op)
-        {
-        case 1:
-            int x, *ptrx; // Declarar entero 'x' y puntero a entero 'ptr'
-            ptrx = &x;    // Asignar puntero ptr a dirección de 'x'
-
-            x = 10;
-
-            printf("X: %d, *ptr: %d", x, *ptrx);
-            break;
-
-        case 2:
-            int y, *ptry;
-            ptry = &y;
-
-            y = 10;
-
-            printf("Y antes: %d\n", y);
-
-            multiplicarPorDos(ptry);
-
-            printf("Y despues: %d", y);
-            break;
-
-        case 3:
-            int arr[7], num;
-
-            for (i = 0; i < 7; i++)
-            {
-                do
-                {
-                    num = numAleatorio(0, 50);
-                } while (busqSeqVect(arr, 7, num) != -1);
-
-                arr[i] = num;
-            }
-
-            int *ptrArr = arr;
-
-            for (i = 0; i < 7; i++)
-            {
-                printf("%d, ", *ptrArr);
-                ptrArr++;
-            }
-            break;
-
-        case 4:
-
-            break;
-
-        case 0:
-            return 0;
-        }
-
-        printf("\n");
-        system("PAUSE");
-    } while (op != 0);
-
-    return 0;
-}
-
 void multiplicarPorDos(int *a)
 {
     *a = (*a) * 2;
+}
+
+void sumarAElementos(int *arr, int n, int val)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        *arr += val;
+        arr++;
+    }
 }
